432909ddc557ba45653ff0e32e8160ae7a549e2c
jareer <jareerzeenam@adventus.io>
2022-11-01 01:21:12 +0530

    Timeline component added with basic stylings using  Bulma css

---
+++ src/components/Timeline.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const periods = ["Today", "This Week", "This Month", "This Year"];
+
+const selectedPeriod = ref("Today");
+
+const selectPeriod = (period: string) => {
+  selectedPeriod.value = period;
+};
+</script>
+
+<template>
+  <nav class="is-primary panel">
+    {{ selectedPeriod }}
+    <span class="panel-tabs">
+      <a
+        v-for="period of periods"
+        :key="period"
+        :class="{ 'is-active': period === selectedPeriod }"
+        @click="selectPeriod(period)"
+      >
+        {{ period }}
+      </a>
+    </span>
+  </nav>
+</template>
