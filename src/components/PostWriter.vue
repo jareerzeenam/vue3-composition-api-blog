4b447a69c9661f534d4980fcfa42da913abb1d9a
jareer <jareerzeenam@adventus.io>
2022-11-03 00:20:17 +0530

    26. Tow way binding with v-model done

---
+++ src/components/PostWriter.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import { TimelinePost } from "../posts";
+
+const props = defineProps<{
+  post: TimelinePost;
+}>();
+
+const title = ref(props.post.title);
+</script>
+
+<template>
+  <div class="columns">
+    <div class="column">
+      <div class="field">
+        <div class="label">Post title</div>
+        <input type="text" class="input" v-model="title" />
+        {{ title }}
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,1 +1,19 @@
-<template>New Post</template>
+<script lang="ts" setup>
+import { DateTime } from "luxon";
+import PostWriter from "../components/PostWriter.vue";
+import { TimelinePost } from "../posts";
+
+const post: TimelinePost = {
+  id: "-1",
+  title: "Title",
+  description: "Description",
+  createdAt: DateTime.now(),
+  author: "d5f4d5sf",
+  categoryId: "sdssfdf",
+};
+</script>
+
+<template>
+  New Post
+  <PostWriter :post="post" />
+</template>
