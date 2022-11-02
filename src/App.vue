0bca090dae328f16a9fa5ec39080d4e68ad79bbf
jareer <jareerzeenam@adventus.io>
2022-11-02 23:37:23 +0530

    21. Loading state with suspense added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
-import Timeline from './components/Timeline.vue';
+import Timeline from "./components/Timeline.vue";
 </script>
 
 <template>
   <div class="section">
     <div class="container">
-      <Timeline />
+      <Suspense>
+        <template #default>
+          <Timeline />
+        </template>
+        <template #fallback>
+          <progress class="progress is-primary is-small" />
+        </template>
+      </Suspense>
     </div>
   </div>
 </template>
 
 <style>
-@import 'https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css';
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
 </style>
