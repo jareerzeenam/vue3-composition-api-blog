69545bd86926531d0a98ec4909a9618fe7e1db12
jareer <jareerzeenam@adventus.io>
2022-11-08 23:38:09 +0530

    47. Signing a JWT done

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,14 @@
-import { NewUser, User } from './../users';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import jsonwebtoken from 'jsonwebtoken';
 import { today, thisWeek, thisMonth, thisYear, Post } from '../posts';
+import { NewUser, User } from './../users';
 
 const app = express();
 app.use(cors());
+app.use(cookieParser());
 app.use(bodyParser.json());
 
 const allPosts = [today, thisWeek, thisMonth, thisYear];
@@ -21,9 +24,28 @@ app.post<{}, {}, Post>('/posts', (req, res) => {
   res.json(post);
 });
 
+// Move this to environment variable (.env)
+const SECRET = 'my-secret';
+const COOKIE = 'vuejs-jwt';
+
+//! Authentication
+function authentication(id: string, req: Request, res: Response) {
+  // create a token and sign it
+  const token = jsonwebtoken.sign({ id }, SECRET, {
+    issuer: 'vuejs-course', // Whoever creates the token
+    expiresIn: '30 days', // expires in 30 days
+  });
+
+  //set a cookie
+  res.cookie(COOKIE, token, { httpOnly: true });
+}
+
 app.post<{}, {}, NewUser>('/users', (req, res) => {
   const user: User = { ...req.body, id: (Math.random() * 100000).toFixed() };
   allUsers.push(user);
+
+  // Auth
+  authentication(user.id, req, res);
   // Ignore password and return the rest
   const { password, ...rest } = user;
   res.json(rest);
