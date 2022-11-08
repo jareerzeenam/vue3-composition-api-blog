e457c5edd42cca1cea175a43d31f6b19eb6f1e2b
jareer <jareerzeenam@adventus.io>
2022-11-08 22:59:09 +0530

    44. Node.js user creation endpoint added

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,4 @@
+import { NewUser, User } from './../users';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const allPosts = [today, thisWeek, thisMonth, thisYear];
+const allUsers: User[] = [];
 
 app.get('/posts', (req, res) => {
   res.json(allPosts);
@@ -17,6 +19,14 @@ app.post<{}, {}, Post>('/posts', (req, res) => {
   const post = { ...req.body, id: (Math.random() * 100000).toFixed() };
   allPosts.push(post);
   res.json(post);
+});
+
+app.post<{}, {}, NewUser>('/users', (req, res) => {
+  const user: User = { ...req.body, id: (Math.random() * 100000).toFixed() };
+  allUsers.push(user);
+  // Ignore password and return the rest
+  const { password, ...rest } = user;
+  res.json(rest);
 });
 
 app.listen(5500, () => {
