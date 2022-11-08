2ad1057dcf58a842c2fca2ffe5308ee548e3ed16
jareer <jareerzeenam@adventus.io>
2022-11-08 20:35:43 +0530

    40. Agnostic Validation Done

--- src/validation.ts
+++ src/validation.ts
@@ -5,14 +5,28 @@ interface Status {
 
 type Rule = (value: string) => Status;
 
-export function required(value: string): Status {
+export function length({ min, max }: { min: number; max: number }): Rule {
+  return function (value: string): Status {
+    const result = Boolean(value.length > min && value.length < max);
+
+    return {
+      valid: result,
+      message: result
+        ? undefined
+        : `This field must be between ${min} and ${max}`,
+    };
+  };
+}
+
+// validate required
+export const required: Rule = (value: string): Status => {
   const result = Boolean(value);
 
   return {
     valid: result,
     message: result ? undefined : 'This field is required!',
   };
-}
+};
 
 export function validate(value: string, rules: Rule[]): Status {
   for (const rule of rules) {
@@ -28,4 +42,8 @@ export function validate(value: string, rules: Rule[]): Status {
   };
 }
 
-console.log(validate('', [required]), validate('username', [required]));
+console.log(
+  validate('a', [length({ min: 5, max: 10 })]),
+  validate('aaaaaaaaaaaa', [length({ min: 5, max: 10 })]),
+  validate('username', [length({ min: 5, max: 10 })])
+);
