5ccf0cdf8b69009307c00d47f3cf9e39d6ab56fe
jareer <jareerzeenam@adventus.io>
2022-11-09 20:15:00 +0530

    61. Conditionally Update Post

--- src/views/ShowPost.vue
+++ src/views/ShowPost.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup>
-import { useRoute } from "vue-router";
-import { usePosts } from "../stores/posts";
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import { usePosts } from '../stores/posts';
+import { useUsers } from '../stores/users';
 
 const route = useRoute();
 const postsStore = usePosts();
+const usersStore = useUsers();
 
 const id = route.params.id as string;
 const post = postsStore.all.get(id);
@@ -11,13 +14,29 @@ const post = postsStore.all.get(id);
 if (!post) {
   throw Error(`Post with id ${id} was not found!`);
 }
+
+const canEdit = computed(() => {
+  if (!usersStore.currentUserId) {
+    return false;
+  }
+
+  if (usersStore.currentUserId !== post.author) {
+    return false;
+  }
+
+  return true;
+});
 </script>
 
 <template>
   <div class="columns">
     <div class="column"></div>
     <div class="column is-two-thirds">
-      <RouterLink :to="`/posts/${post.id}/edit`" class="is-link button is-rounded">
+      <RouterLink
+        v-if="canEdit"
+        :to="`/posts/${post.id}/edit`"
+        class="is-link button is-rounded"
+      >
         Edit Post
       </RouterLink>
       <h1>{{ post.title }}</h1>
