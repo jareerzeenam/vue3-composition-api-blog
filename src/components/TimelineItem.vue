63e383a9acf79f892c178c714afadbab76114b4b
jareer <jareerzeenam@adventus.io>
2022-11-09 18:07:33 +0530

    58. Show Post done

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -7,10 +7,13 @@ defineProps<{
 </script>
 
 <template>
-  <a class="panel-block is-flex is-flex-direction-column is-align-items-flex-start">
+  <RouterLink
+    :to="`/posts/${post.id}`"
+    class="panel-block is-flex is-flex-direction-column is-align-items-flex-start"
+  >
     <a>{{ post.title }}</a>
     <p>{{ post.description }}</p>
     <div>{{ post.createdAt.toFormat("d MMM") }}</div>
     {{ post.author }}
-  </a>
+  </RouterLink>
 </template>
