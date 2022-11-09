bc76a39f435225c76942797bfedbff2029abf85c
jareer <jareerzeenam@adventus.io>
2022-11-09 23:11:03 +0530

    70. Form Input Test added

--- src/components/FormInput.spec.ts
+++ src/components/FormInput.spec.ts
@@ -1,8 +1,37 @@
 import { mount } from '@vue/test-utils';
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
+import FormInput from './FormInput.vue';
 
 describe('FormInput', () => {
   it('renders some errors', () => {
-    // ...
+    const wrapper = mount(FormInput, {
+      props: {
+        name: 'foo',
+        modelValue: 'bar',
+        status: {
+          valid: false,
+          message: 'error',
+        },
+        type: 'input',
+      },
+    });
+    // console.log(wrapper.html());
+    expect(wrapper.find('.is-danger').exists()).toBe(true);
+  });
+
+  it('renders no errors', () => {
+    const wrapper = mount(FormInput, {
+      props: {
+        name: 'foo',
+        modelValue: 'bar',
+        status: {
+          valid: true,
+          message: 'error',
+        },
+        type: 'input',
+      },
+    });
+    // console.log(wrapper.html());
+    expect(wrapper.find('.is-danger').exists()).toBe(false);
   });
 });
